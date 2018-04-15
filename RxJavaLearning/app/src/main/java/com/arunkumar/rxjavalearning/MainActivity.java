817bc22b263f0d4eec3dc673aae724068600306b
arun <arun.s@allegion.com>
2018-04-15 20:22:30 +0530

    Few usecases for Observable using RxAndroid

--- RxJavaLearning/app/src/main/java/com/arunkumar/rxjavalearning/MainActivity.java
+++ RxJavaLearning/app/src/main/java/com/arunkumar/rxjavalearning/MainActivity.java
@@ -3,138 +3,150 @@ package com.arunkumar.rxjavalearning;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
-import io.reactivex.Single;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
-    /**
-     * Problem statement:
-     * Monk just purchased an array A having N integers. Monk is very superstitious. He calls the
-     * array A Lucky if the frequency of the minimum element is odd, otherwise he considers it
-     * Unlucky. Help Monk in finding out if the array is Lucky or not.
-     */
-
-    int[] arrayA = {8, 8, 5, 5, 5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        FrameLayout row = findViewById(R.id.test);
-
-        View.OnTouchListener l = new View.OnTouchListener() {
-            int count = 0;
-
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                count++;
-                Log.v("", "count: " + count);
-                if(count == 1) {
-                    Friend.main();
-                }
-                return true;
-            }
-        };
-
-        row.setOnTouchListener(l);
-
-
-
-        createAObservable();
-        createAObservableWithArray();
+        addNumbers();
+        loopThroughArray();
+        multipleObservablesUsecase();
+        multipleOddEven();
+        printNumbers();
     }
 
-    private void createAObservable() {
-        int data = 40;
-        Observable<Integer> observable = Observable.just(data);
-        observable.subscribe(newData -> Log.v("", "new data: " + newData));
-    }
-
-    private void createAObservableWithArray() {
+    /**
+     * Add numbers passed on from observable
+     */
+    private void addNumbers() {
         Observable
-                .fromIterable(datas())
-                .observeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(d -> Log.v("", "d is : " + d));
+                .just(1, 2, 3)
+                .reduce((a, b) -> a + b)
+                .subscribe(
+                        result -> Log.v("", "addNumbers -> " + result),
+                        error -> error.printStackTrace()
+                );
     }
 
-    private Observable<Integer> test() {
-        return Observable.fromIterable(datas2());
+    /**
+     * Same as above but takes arraylist as input and adds all element and gives total
+     */
+    private void loopThroughArray() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        Observable
+                .fromIterable(numbers)
+                .reduce((a, b) -> a + b)
+                .subscribe(
+                        result -> Log.v("", "loopThroughArray -> " + result),
+                        error -> error.printStackTrace()
+                );
     }
 
-    private List<Integer> datas() {
-        List<Integer> data = new ArrayList<>();
-        data.add(1);
-        data.add(2);
-        data.add(3);
-        data.add(4);
-        data.add(5);
-        return data;
+    /**
+     * Observable A[0, 1, 2]
+     * Observable B[2, 4, 6]
+     * <p>
+     * Divide all elements of B with all elements of A and avoid Divide by zero exception
+     * Ex result:
+     * 2/1, 2/2, 4/1, 4/2, 6/1, 6/2 => 2, 1, 4, 2, 6, 3
+     */
+
+    private void multipleObservablesUsecase() {
+        Observable<Integer> num1 = Observable.just(0, 1, 2);
+        Observable<Integer> num2 = Observable.just(2, 4, 6);
+        Observable<String> letters = Observable.just("a", "b");
+
+        num2.flatMap(
+                i2 -> num1.filter(i -> i != 0)
+                        .map(i1 -> i2 / i1)
+        ).subscribe(
+                s -> Log.v("", "cartisianProduct -> " + s),
+                error -> error.printStackTrace()
+        );
     }
 
-    private List<Integer> datas2() {
-        List<Integer> data = new ArrayList<>();
-        data.add(6);
-        data.add(7);
-        data.add(8);
-        data.add(9);
-        data.add(0);
-        return data;
+    /**
+     * [1, 2, 3, 4, 5] - 3 odds, 2 evens
+     * Requirement: Find number of odds and evens and multiple odd count with odd numbers
+     * & even count with even numbers and print them.
+     * Ex result:
+     * 3   (1 * 3)
+     * 4   (2 * 2)
+     * 9   (3 * 3)
+     * 8   (4 * 2)
+     * 15  (5 * 3)
+     */
+    private void multipleOddEven() {
+        Observable<Integer> obs = Observable.just(1, 2, 3, 4, 5);
+
+        Observable<Integer> evenCount = obs.filter(integer -> integer % 2 == 0);
+        Observable<Integer> oddCount = obs.filter(integer -> integer % 2 != 0);
+
+
+        obs.flatMap(
+                integer -> {
+                    if (integer % 2 == 0) {
+                        return evenCount.count().map(count -> count * integer).toObservable();
+                    } else {
+                        return oddCount.count().map(count -> count * integer).toObservable();
+                    }
+                }
+        ).subscribe(
+                s -> Log.v("", "multipleOddEven -> " + s),
+                error -> error.printStackTrace()
+        );
     }
 
-    private void checkIfLucky() {
-        Single.just(arrayA)
-                .map(arrayElement -> {
-                            int min = arrayElement[0];
-                            int count = 1;
-                            for (int i = 1; i < arrayElement.length; i++) {
-                                if (min > arrayElement[i]) {
-                                    min = arrayElement[i];
-                                    count = 1;
-                                } else if (min == arrayElement[i]) {
-                                    count++;
-                                }
+    /**
+     * Iterate through integer array and print each element
+     */
+    private void printNumbers() {
+        List<Integer> arrayIntegers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            arrayIntegers.add(i);
+        }
+
+        Observable
+                .fromIterable(arrayIntegers)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        new Observer<Integer>() {
+                            @Override
+                            public void onSubscribe(Disposable d) {
+
                             }
-                            if (count % 2 != 0) {
-                                return "Lucky";
-                            } else {
-                                return "Unlucky";
+
+                            @Override
+                            public void onNext(Integer i) {
+                                Log.d("", "printNumbers -> " + i);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                Log.d("", "printNumbers -> Completed");
                             }
                         }
-                ).subscribe(
-                isLucky -> Log.d("", "He is " + isLucky),
-                error -> error.printStackTrace());
+                );
     }
-
-    public boolean isPalindrome(String word) {
-        word = word.toLowerCase();
-        if (word.length() % 2 != 0) {
-            return false;
-        }
-        int j = word.length();
-        boolean isPalindrom = true;
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != word.charAt(j)) {
-                isPalindrom = false;
-                break;
-            } else {
-                j--;
-            }
-        }
-        return isPalindrom;
-    }
-
 }
